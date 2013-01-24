433e1d3c46b40591cc92788419025aa7e8652442
Michael Behan <djether@gmail.com>
2013-01-24 16:22:26 -0500

    Adding chainability to Itinerary object methods

--- SDI/Project3/behan-michael-assignment3.js
+++ SDI/Project3/behan-michael-assignment3.js
@@ -1,1 +1,44 @@
-alert("JavaScript works!");
+
+
+// Global variables
+
+
+// FastPass - stores information about a guest's fastpass for a facility
+// Parameters:
+//  attraction - String
+//  time       - String
+var FastPass = function(attraction, time) {
+    this.attraction = attraction;
+    this.time = time;
+};
+
+// Itinerary class; stores information about a guest's itinerary
+// Parameters:
+//  themePark: String
+var Itinerary = function(themePark) {
+    var park       = '',
+        fastPasses = [];
+
+    park = themePark;
+
+    // Parameter:
+    //  fastPass (FastPass instance)
+    function addFastPass(fastPass) {
+        fastPasses.push(fastPass);
+        return this;
+    }
+
+    function list() {
+        console.log('Itinerary:');
+        for (var i in fastPasses) {
+            console.log(fastPasses[i].attraction + ' at ' + fastPasses[i].time)
+        }
+        return this;
+    }
+
+    return {
+      addFastPass: addFastPass,
+      list: list,
+      park: themePark
+    };
+};
