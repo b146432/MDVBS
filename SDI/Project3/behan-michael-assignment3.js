5c9ebf6a92130db3c087ce4ee3a39ffd1ff764aa
Michael Behan <mbehan@fullsail.edu>
2013-01-24 18:30:05 -0500

    Adding initial Graffle graph; Adding variable assignments to JS; lots of code tweaking

--- SDI/Project3/behan-michael-assignment3.js
+++ SDI/Project3/behan-michael-assignment3.js
@@ -1,44 +1,134 @@
+// SDI 1301 Project 3
+// Michael Behan
 
 
 // Global variables
 
-
-// FastPass - stores information about a guest's fastpass for a facility
-// Parameters:
-//  attraction - String
-//  time       - String
-var FastPass = function(attraction, time) {
-    this.attraction = attraction;
-    this.time = time;
+// Itinerary JSON
+var json = {
+    "itinerary": [
+        {
+            "park": "Magic Kingdom",
+            "fastPasses": [
+                {
+                    "location": "Space Mountain",
+                    "arrival": "2:00pm-2:25pm"
+                },
+                {
+                    "location": "Celebrate a Dream Come True Parade",
+                    "arrival": "2:45pm-3:00pm"
+                }
+            ]
+        },
+        {
+            "park": "Hollywood Studios",
+            "fastPasses": [
+                {
+                    "location": "Twilight Zone Tower of Terror",
+                    "arrival": "10:00am-10:15am"
+                }
+            ]
+        }
+    ]
 };
 
-// Itinerary class; stores information about a guest's itinerary
-// Parameters:
-//  themePark: String
-var Itinerary = function(themePark) {
-    var park       = '',
-        fastPasses = [];
+// FastPass Object
+var FastPass = function(place, time) {
+    var location = place,
+        arrival  = time;
 
-    park = themePark;
+    return {
+        location: location,
+        arrival: arrival
+    };
+};
 
+// Itinerary Object
+var Itinerary = function() {
+    var rawData       = null,
+        parks         = [],
+        fastPasses    = [],
+        fastPassCount = 0,
+        favoritePark  = '',
+        hasData       = false;
+
+    function getHasData() {
+        return hasData;
+    }
+
+    function getFavoritePark() {
+        return favoritePark;
+    }
+
+    // Initialize from data
     // Parameter:
-    //  fastPass (FastPass instance)
-    function addFastPass(fastPass) {
-        fastPasses.push(fastPass);
-        return this;
+    //  data (raw JSON)
+    function init(data) {
+        rawData = data;
+
+        for (var i = 0, len = json.itinerary.length; i < len; i++) {
+            parks.push(json.itinerary[i].park);
+
+            for (var j = 0, len2 = json.itinerary[i].fastPasses.length; j < len2; j++) {
+                var fastPass = json.itinerary[i].fastPasses[j];
+                fastPasses.push(fastPass);
+            }
+        }
+
+        hasData = true;
+        console.log('Itinerary Initialized');
+    }
+
+    function getParks() {
+        if (parks.length > 0) {
+            return parks;
+        } else {
+            return null;
+        }
+    }
+
+    function setFavoritePark(name) {
+        favoritePark = name;
     }
 
     function list() {
-        console.log('Itinerary:');
-        for (var i in fastPasses) {
-            console.log(fastPasses[i].attraction + ' at ' + fastPasses[i].time)
+        console.log('Itinerary List:');
+        if (parks.length > 0) {
+            if (fastPasses.length > 0) {
+
+                for (var j = 0, len2 = fastPasses.length; j < len2; j++) {
+                    console.log("\t" + fastPasses[j].location + ' ' + fastPasses[j].arrival);
+                }
+            }
         }
-        return this;
+    }
+
+    function getFastPass(number) {
+        return fastPasses[number] || 'Not Found';
     }
 
     return {
-      addFastPass: addFastPass,
-      list: list,
-      park: themePark
+        getHasData: getHasData,
+        init: init,
+        rawData: rawData,
+        getParks: getParks,
+        setFavoritePark: setFavoritePark,
+        favoritePark: favoritePark,
+        list: list,
+        getFastPass: getFastPass,
+        getFavoritePark: getFavoritePark
     };
 };
+
+var i = new Itinerary();
+console.log('Itinerary has data', i.getHasData())
+i.init(json);
+console.log('Itinerary has data', i.getHasData())
+i.list();
+i.setFavoritePark('Magic Kingdom');
+var favorite = i.getFavoritePark();
+var fastPass2 = i.getFastPass(1);
+
+console.log('Favorite Park: ', favorite);
+console.log('Second FastPass Location: ', fastPass2.location);
+
