865e0985dd04bff63dde5bc65911336c26b33a66
Michael Behan <michaeljbehan@me.com>
2013-03-12 19:20:40 -0400

    Add Gold & Bronze Apps

---
+++ MIU/behan_michael_project2/Bronze/js/JSON.js
@@ -0,0 +1,222 @@
+var json = [
+    {
+        "game": "Skyrim",
+        "category": "glitch",
+        "systems": "ps3,xbox,pc",
+        "code": "N/A",
+        "author": "The Bobs",
+        "ease": 20,
+        "thorough": "on",
+        "description": "Skyrim - Dragonborn DLC. This is pretty awesome. Just walk through the left wall in Windhelm.",
+        "date": "2013-02-28"
+    },
+    {
+        "game": "Red Dead Redemptiomn",
+        "category": "cheatcode",
+        "systems": "ps3,xbox",
+        "code": "N/A",
+        "author": "Michael Berhan",
+        "ease": 50,
+        "thorough": "on",
+        "description": "Get all the outfits and you will get a better one to boot.",
+        "date": "2013-02-28"
+    },
+    {
+        "game": "Dragon Age",
+        "category": "glitch",
+        "systems": "ps3",
+        "code": "N/A",
+        "author": "Jenny",
+        "ease": 75,
+        "thorough": "on",
+        "description": "Talk to Duncan 5 times then attack him.",
+        "date": "2013-02-28"
+    },
+    {
+        "game": "Crash Bandicoot",
+        "category": "secret",
+        "systems": "ps3",
+        "code": "N/A",
+        "author": "The Bobs",
+        "ease": 1,
+        "thorough": "on",
+        "description": "Find all gems in every stage.",
+        "date": "2013-02-28"
+    },
+    {
+        "game": "Contra",
+        "category": "secret",
+        "systems": "wii",
+        "code": "Up up Down Down left right left right B A Start",
+        "author": "Hunter S. Thompson",
+        "ease": 1,
+        "thorough": "on",
+        "description": "Stage select on title. Input this code on the title screen.",
+        "date": "2013-02-28"
+    },
+    {
+        "game": "Super Mario Bros 3 (Wii Edition)",
+        "category": "glitch",
+        "systems": "wii,wiiu",
+        "code": "N/A",
+        "author": "Team America",
+        "ease": 20,
+        "thorough": "on",
+        "description": "In world 3-1, duck on the second white box until you fall through the floor. Then run to the end for a warp whistle.",
+        "date": "2013-02-28"
+    },
+    {
+        "game": "Skyrim",
+        "category": "secret",
+        "systems": "ps3,xbox",
+        "code": "N/A",
+        "author": "Michael Berhan",
+        "ease": 50,
+        "thorough": "on",
+        "description": "Level up the Sneak skill easily by slashing the first NPC that guides you into the Keep at the beginning of the game.",
+        "date": "2013-02-28"
+    },
+    {
+        "game": "Grand Turismo",
+        "category": "cheatcode",
+        "systems": "ps3",
+        "code": "N/A",
+        "author": "Jenny",
+        "ease": 75,
+        "thorough": "on",
+        "description": "Talk to Duncan 5 times then attack him.",
+        "date": "2013-02-28"
+    },
+    {
+        "game": "Crash Bandicoot",
+        "category": "secret",
+        "systems": "ps3",
+        "code": "N/A",
+        "author": "The Bobs",
+        "ease": 1,
+        "thorough": "on",
+        "description": "Find all gems in every stage.",
+        "date": "2013-02-28"
+    },
+    {
+        "game": "Contra",
+        "category": "secret",
+        "systems": "wii",
+        "code": "Up up Down Down left right left right B A Start",
+        "author": "Hunter S. Thompson",
+        "ease": 1,
+        "thorough": "on",
+        "description": "Stage select on title. Input this code on the title screen.",
+        "date": "2013-02-28"
+    },
+    {
+        "game": "Skyrim",
+        "category": "glitch",
+        "systems": "ps3,xbox,pc",
+        "code": "N/A",
+        "author": "The Bobs",
+        "ease": 20,
+        "thorough": "on",
+        "description": "Skyrim - Dragonborn DLC. This is pretty awesome. Just walk through the left wall in Windhelm.",
+        "date": "2013-02-28"
+    },
+    {
+        "game": "Red Dead Redemptiomn",
+        "category": "cheatcode",
+        "systems": "ps3,xbox",
+        "code": "N/A",
+        "author": "Michael Berhan",
+        "ease": 50,
+        "thorough": "on",
+        "description": "Get all the outfits and you will get a better one to boot.",
+        "date": "2013-02-28"
+    },
+    {
+        "game": "Dragon Age",
+        "category": "glitch",
+        "systems": "ps3",
+        "code": "N/A",
+        "author": "Jenny",
+        "ease": 75,
+        "thorough": "on",
+        "description": "Talk to Duncan 5 times then attack him.",
+        "date": "2013-02-28"
+    },
+    {
+        "game": "Crash Bandicoot",
+        "category": "secret",
+        "systems": "ps3",
+        "code": "N/A",
+        "author": "The Bobs",
+        "ease": 1,
+        "thorough": "on",
+        "description": "Find all gems in every stage.",
+        "date": "2013-02-28"
+    },
+    {
+        "game": "Contra",
+        "category": "secret",
+        "systems": "wii",
+        "code": "Up up Down Down left right left right B A Start",
+        "author": "Hunter S. Thompson",
+        "ease": 1,
+        "thorough": "on",
+        "description": "Stage select on title. Input this code on the title screen.",
+        "date": "2013-02-28"
+    },
+    {
+        "game": "Super Mario Bros 3 (Wii Edition)",
+        "category": "glitch",
+        "systems": "wii,wiiu",
+        "code": "N/A",
+        "author": "Team America",
+        "ease": 20,
+        "thorough": "on",
+        "description": "In world 3-1, duck on the second white box until you fall through the floor. Then run to the end for a warp whistle.",
+        "date": "2013-02-28"
+    },
+    {
+        "game": "Skyrim",
+        "category": "secret",
+        "systems": "ps3,xbox",
+        "code": "N/A",
+        "author": "Michael Berhan",
+        "ease": 50,
+        "thorough": "on",
+        "description": "Level up the Sneak skill easily by slashing the first NPC that guides you into the Keep at the beginning of the game.",
+        "date": "2013-02-28"
+    },
+    {
+        "game": "Grand Turismo",
+        "category": "cheatcode",
+        "systems": "ps3",
+        "code": "N/A",
+        "author": "Jenny",
+        "ease": 75,
+        "thorough": "on",
+        "description": "Talk to Duncan 5 times then attack him.",
+        "date": "2013-02-28"
+    },
+    {
+        "game": "Crash Bandicoot",
+        "category": "secret",
+        "systems": "ps3",
+        "code": "N/A",
+        "author": "The Bobs",
+        "ease": 1,
+        "thorough": "on",
+        "description": "Find all gems in every stage.",
+        "date": "2013-02-28"
+    },
+    {
+        "game": "Contra",
+        "category": "secret",
+        "systems": "wii",
+        "code": "Up up Down Down left right left right B A Start",
+        "author": "Hunter S. Thompson",
+        "ease": 1,
+        "thorough": "on",
+        "description": "Stage select on title. Input this code on the title screen.",
+        "date": "2013-02-28"
+    }
+];
