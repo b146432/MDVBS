3b3efeab8b81f89a304bebc7ed47c98e21f445b5
Michael Behan <michaeljbehan@me.com>
2013-03-14 18:59:13 -0400

    Too much to document

--- MIU/behan_michael_project2/Gold/js/JSON.js
+++ MIU/behan_michael_project2/Gold/js/JSON.js
@@ -5,18 +5,16 @@ var json = [
         "systems": "ps3,xbox,pc",
         "code": "N/A",
         "author": "The Bobs",
-        "ease": 20,
         "thorough": "on",
         "description": "Skyrim - Dragonborn DLC. This is pretty awesome. Just walk through the left wall in Windhelm.",
         "date": "2013-02-28"
     },
     {
-        "game": "Red Dead Redemptiomn",
+        "game": "Red Dead Redemption",
         "category": "cheatcode",
         "systems": "ps3,xbox",
         "code": "N/A",
-        "author": "Michael Berhan",
-        "ease": 50,
+        "author": "Michael Behan",
         "thorough": "on",
         "description": "Get all the outfits and you will get a better one to boot.",
         "date": "2013-02-28"
@@ -27,7 +25,6 @@ var json = [
         "systems": "ps3",
         "code": "N/A",
         "author": "Jenny",
-        "ease": 75,
         "thorough": "on",
         "description": "Talk to Duncan 5 times then attack him.",
         "date": "2013-02-28"
@@ -38,7 +35,6 @@ var json = [
         "systems": "ps3",
         "code": "N/A",
         "author": "The Bobs",
-        "ease": 1,
         "thorough": "on",
         "description": "Find all gems in every stage.",
         "date": "2013-02-28"
@@ -49,7 +45,6 @@ var json = [
         "systems": "wii",
         "code": "Up up Down Down left right left right B A Start",
         "author": "Hunter S. Thompson",
-        "ease": 1,
         "thorough": "on",
         "description": "Stage select on title. Input this code on the title screen.",
         "date": "2013-02-28"
@@ -60,7 +55,6 @@ var json = [
         "systems": "wii,wii-u",
         "code": "N/A",
         "author": "Team America",
-        "ease": 20,
         "thorough": "on",
         "description": "In world 3-1, duck on the second white box until you fall through the floor. Then run to the end for a warp whistle.",
         "date": "2013-02-28"
@@ -70,8 +64,7 @@ var json = [
         "category": "secret",
         "systems": "ps3,xbox",
         "code": "N/A",
-        "author": "Michael Berhan",
-        "ease": 50,
+        "author": "Michael Behan",
         "thorough": "on",
         "description": "Level up the Sneak skill easily by slashing the first NPC that guides you into the Keep at the beginning of the game.",
         "date": "2013-02-28"
@@ -82,7 +75,6 @@ var json = [
         "systems": "ps3",
         "code": "N/A",
         "author": "Jenny",
-        "ease": 75,
         "thorough": "on",
         "description": "Talk to Duncan 5 times then attack him.",
         "date": "2013-02-28"
@@ -93,7 +85,6 @@ var json = [
         "systems": "ps3",
         "code": "N/A",
         "author": "The Bobs",
-        "ease": 1,
         "thorough": "on",
         "description": "Find all gems in every stage.",
         "date": "2013-02-28"
@@ -104,7 +95,6 @@ var json = [
         "systems": "wii",
         "code": "Up up Down Down left right left right B A Start",
         "author": "Hunter S. Thompson",
-        "ease": 1,
         "thorough": "on",
         "description": "Stage select on title. Input this code on the title screen.",
         "date": "2013-02-28"
@@ -115,18 +105,16 @@ var json = [
         "systems": "ps3,xbox,pc",
         "code": "N/A",
         "author": "The Bobs",
-        "ease": 20,
         "thorough": "on",
         "description": "Skyrim - Dragonborn DLC. This is pretty awesome. Just walk through the left wall in Windhelm.",
         "date": "2013-02-28"
     },
     {
-        "game": "Red Dead Redemptiomn",
+        "game": "Red Dead Redemption",
         "category": "cheatcode",
         "systems": "ps3,xbox",
         "code": "N/A",
-        "author": "Michael Berhan",
-        "ease": 50,
+        "author": "Michael Behan",
         "thorough": "on",
         "description": "Get all the outfits and you will get a better one to boot.",
         "date": "2013-02-28"
@@ -137,7 +125,6 @@ var json = [
         "systems": "ps3",
         "code": "N/A",
         "author": "Jenny",
-        "ease": 75,
         "thorough": "on",
         "description": "Talk to Duncan 5 times then attack him.",
         "date": "2013-02-28"
@@ -148,7 +135,6 @@ var json = [
         "systems": "ps3",
         "code": "N/A",
         "author": "The Bobs",
-        "ease": 1,
         "thorough": "on",
         "description": "Find all gems in every stage.",
         "date": "2013-02-28"
@@ -159,7 +145,6 @@ var json = [
         "systems": "wii",
         "code": "Up up Down Down left right left right B A Start",
         "author": "Hunter S. Thompson",
-        "ease": 1,
         "thorough": "on",
         "description": "Stage select on title. Input this code on the title screen.",
         "date": "2013-02-28"
@@ -170,7 +155,6 @@ var json = [
         "systems": "wii,wii-u",
         "code": "N/A",
         "author": "Team America",
-        "ease": 20,
         "thorough": "on",
         "description": "In world 3-1, duck on the second white box until you fall through the floor. Then run to the end for a warp whistle.",
         "date": "2013-02-28"
@@ -180,8 +164,7 @@ var json = [
         "category": "secret",
         "systems": "ps3,xbox",
         "code": "N/A",
-        "author": "Michael Berhan",
-        "ease": 50,
+        "author": "Michael Behan",
         "thorough": "on",
         "description": "Level up the Sneak skill easily by slashing the first NPC that guides you into the Keep at the beginning of the game.",
         "date": "2013-02-28"
@@ -192,7 +175,6 @@ var json = [
         "systems": "ps3",
         "code": "N/A",
         "author": "Jenny",
-        "ease": 75,
         "thorough": "on",
         "description": "Talk to Duncan 5 times then attack him.",
         "date": "2013-02-28"
@@ -203,7 +185,6 @@ var json = [
         "systems": "ps3",
         "code": "N/A",
         "author": "The Bobs",
-        "ease": 1,
         "thorough": "on",
         "description": "Find all gems in every stage.",
         "date": "2013-02-28"
@@ -214,7 +195,26 @@ var json = [
         "systems": "wii",
         "code": "Up up Down Down left right left right B A Start",
         "author": "Hunter S. Thompson",
-        "ease": 1,
+        "thorough": "on",
+        "description": "Stage select on title. Input this code on the title screen.",
+        "date": "2013-02-28"
+    },
+    {
+        "game": "Super Mario Bros DS",
+        "category": "secret",
+        "systems": "ds",
+        "code": "N/A",
+        "author": "The Bobs",
+        "thorough": "on",
+        "description": "Find all gems in every stage.",
+        "date": "2013-02-28"
+    },
+    {
+        "game": "Kirby's Dreamland",
+        "category": "secret",
+        "systems": "ds",
+        "code": "Up up Down Down left right left right B A Start",
+        "author": "Hunter S. Thompson",
         "thorough": "on",
         "description": "Stage select on title. Input this code on the title screen.",
         "date": "2013-02-28"
